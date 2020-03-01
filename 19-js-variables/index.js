2afe9d0969ec3f7869a64eee9628e8a95512f41c
Tanisha <tasheils23@gmail.com>
2020-02-29 22:52:02 -0700

    19 complete

--- 19-js-variables/index.js
+++ 19-js-variables/index.js
@@ -1,23 +1,15 @@
 const BIRTH_MONTH = "July";
-{
-  console.log = "Month of Birth: " + BIRTH_MONTH;
-}
-const FIRST_NAME = "Tanisha";
+const FIRST_NAME = "Tanisha ";
 const LAST_NAME = "Vlahovic";
 
-
 let city = "Calgary";
-{
-  console.log = "City: " + city;
-}
 let age = "27";
-{
-  console.log = "Age: " + age;
-}
 let nextCity = city;
-{
-  console.log = "Next City: " + nextCity;
-}
-{
-  console.log = `${"Last Name: " + LAST_NAME, "First Name: " + FIRST_NAME`}`
-}
+
+console.log([
+  "Month of Birth: " + BIRTH_MONTH,
+  "City: " + city,
+  "age: " + age,
+  "Next City: " + nextCity
+]);
+console.log(`Full name ${FIRST_NAME + LAST_NAME}`);
